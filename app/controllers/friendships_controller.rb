24bab7573c4a53cd1a1cb783bf3fa64956456c7b
scsercan <canbolatsercan@gmail.com>
2022-01-25 12:10:30 +0300

    application completed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+
+    if current_user.save
+      flash[:notice] = "User followed"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
